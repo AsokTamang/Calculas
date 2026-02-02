4318d5ea420e35859fffa0f33468d77bb9219321
AsokTamang <ashoktmg2058@gmail.com>
2026-02-02 12:23:45 -0500

    lambdify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sympy import *
-
+from sympy.utilities.lambdify import lambdify
+import numpy as np
 print(sqrt(18))
 
 x,y = symbols('x y')
@@ -16,3 +17,9 @@ factorised = factor(expanded)
 print(factorised)
 
 expression.evalf(subs={x:1,y:2})  #here we are substituting the value of x and y with 1 and 2 respectively.
+a_array = np.array([1,2,3])
+
+f_calculation = x ** 2
+f_new_calculations =lambdify(x, f_calculation,'numpy')  #taking x input for f_calculation using module numpy
+print(f_new_calculations(a_array))
+
