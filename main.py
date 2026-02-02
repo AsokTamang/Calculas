b3fae8faf2e164ed93203c9f65a0d6907dce15f8
AsokTamang <ashoktmg2058@gmail.com>
2026-02-02 12:07:49 -0500

    some basic sympy APIs

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print(sqrt(18))
 x,y = symbols('x y')
 expression = 3 * x**2 + y
 print(expression)
+
+additional_expression = x * expression
+print(additional_expression)
+
+expanded = expand(additional_expression)  #we are expanding the expression
+print(expanded)
+
+factorised = factor(expanded)
+print(factorised)
